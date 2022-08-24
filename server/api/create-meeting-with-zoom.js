51958204d290cc129c0ceb359bd869571fbae6d7
Basit Bashir <basitowaisi@gmail.com>
2022-08-24 12:52:37 +0530

    Fix timezone issue on creating meeting

--- server/api/create-meeting-with-zoom.js
+++ server/api/create-meeting-with-zoom.js
@@ -99,8 +99,20 @@ module.exports = async (req, res) => {
 
     const zoomUserId = respData?.id;
 
+    const dateFormat = moment(new Date(startDate))
+      .tz(timezone)
+      .format('YYYY-MM-DD');
+    const timeFormat = moment(new Date(startDate))
+      .tz(timezone)
+      .format('HH:mm:ss');
+
+    const start_time = `${dateFormat}T${timeFormat}`;
+
     const meetingParams = JSON.stringify({
-      start_time: moment(new Date(startDate)).format('YYYY-MM-DDTHH:mm:ss'),
+      // start_time: moment(new Date(startDate))
+      //   .tz(timezone)
+      //   .format('YYYY-MM-DDTHH:mm:ss'),
+      start_time,
       timezone: timezone,
       type: 2,
       duration,
