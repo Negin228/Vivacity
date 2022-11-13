743693cd9fecc1dc692d089304e1e13bb5947090
Negin228 <99446649+Negin228@users.noreply.github.com>
2022-11-13 17:14:35 -0600

    Update EditListingDescriptionForm.js

--- src/forms/EditListingDescriptionForm/EditListingDescriptionForm.js
+++ src/forms/EditListingDescriptionForm/EditListingDescriptionForm.js
@@ -254,7 +254,7 @@ const EditListingDescriptionFormComponent = props => (
             className={css.title}
             id="stock"
             name="stock"
-            label="Capacity"
+            label="Maximum Capacity"
             placeholder="How many students can attend this class?"
             type="number"
             min={0}
