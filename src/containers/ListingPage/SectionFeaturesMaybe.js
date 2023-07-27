f909c54e5b62875c966a98f2945f3d1fba10165d
shabir nazir <66513327+shabirnazir@users.noreply.github.com>
2023-07-27 13:55:30 +0530

    test timezone

--- src/containers/ListingPage/SectionFeaturesMaybe.js
+++ src/containers/ListingPage/SectionFeaturesMaybe.js
@@ -19,9 +19,17 @@ const SectionFeaturesMaybe = props => {
   );
 
   // prettier-ignore
-
-  const formattedDate = moment(publicData.startDate).tz(publicData.timezone, true).local().format('dddd, MMMM Do YYYY, h:mm a')
-
+  const myTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  console.log({ myTimeZone });
+  const formattedDate = moment(publicData.startDate)
+    .tz(publicData.timezone, true)
+    .local()
+    .format('dddd, MMMM Do YYYY, h:mm a');
+  const convertedTime = moment(publicData.startDate)
+    .tz(publicData.timezone, true)
+    .tz(myTimeZone)
+    .format('dddd, MMMM Do YYYY, h:mm a');
+  console.log({ convertedTime });
   return (
     <div className={css.sectionFeatures}>
       <h2 className={css.featuresTitle}>
