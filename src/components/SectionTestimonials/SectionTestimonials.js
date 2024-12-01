2e62ba2d55acc7e121fdb7a48b993a35f662b711
Negin228 <99446649+Negin228@users.noreply.github.com>
2024-12-01 10:34:52 -0800

    Update SectionTestimonials.js

--- src/components/SectionTestimonials/SectionTestimonials.js
+++ src/components/SectionTestimonials/SectionTestimonials.js
@@ -1,1 +1,82 @@
+import React from 'react';
+import { bool, string } from 'prop-types';
+import classNames from 'classnames';
+import { FormattedMessage } from '../../util/reactIntl';
+import { propTypes } from '../../util/types';
+import { OwnListingLink } from '../../components';
+import { NamedLink } from '../../components';
+import css from './SectionHowItWorks.module.css';
 
+const SectionHowItWorks = props => {
+  const { rootClassName, className, currentUserListing, currentUserListingFetched } = props;
+  const routeLink = (name, text) => {
+    return (
+      <NamedLink name={name}>
+        <span>{text}</span>
+      </NamedLink>
+    );
+  };
+  const classes = classNames(rootClassName || css.root, className);
+  return (
+    <div className={classes}>
+      <div className={css.title}>
+        <FormattedMessage id="SectionHowItWorks.titleLineOne" />
+      </div>         
+      <div className={css.content}>
+        <FormattedMessage id="SectionHowItWorks.titleLineTwo" />
+      </div>
+      <div className={css.content}>
+        <FormattedMessage id="SectionHowItWorks.titleLineThree" />
+      </div>
+
+      <div className={css.steps}>
+        <div className={css.step}>
+          <h2 className={css.stepTitle}>
+            <FormattedMessage id="SectionHowItWorks.part1Title" />
+          </h2>
+          <p>
+            Start by {routeLink('SignupPage', 'signing up')} as a student. Then browse our catalogue and find the live online workout class that fits in your schedule. Simply book the session and enjoy! All our classes are live, engaging, and most importantly, fun!
+          </p>
+        </div>
+
+        <div className={css.step}>
+          <h2 className={css.stepTitle}>
+            <FormattedMessage id="SectionHowItWorks.part2Title" />
+          </h2>
+          <p>
+            <FormattedMessage id="SectionHowItWorks.part2Text" />
+          </p>
+        </div>
+
+        <div className={css.step}>
+          <h2 className={css.stepTitle}>
+            <FormattedMessage id="SectionHowItWorks.part3Title" />
+          </h2>
+          <p>
+            <FormattedMessage id="SectionHowItWorks.part3Text" />
+          </p>
+        </div>
+      </div>
+      <div className={css.createListingLink}>
+        {routeLink('SearchPage', 'Are you an instructor? Schedule a class!')}
+        {/* <FormattedMessage id="SectionHowItWorks.createListingLink" /> */}
+      </div>
+    </div>
+  );
+};
+
+SectionHowItWorks.defaultProps = {
+  rootClassName: null,
+  className: null,
+  currentUserListing: null,
+  currentUserListingFetched: false,
+};
+
+SectionHowItWorks.propTypes = {
+  rootClassName: string,
+  className: string,
+  currentUserListing: propTypes.ownListing,
+  currentUserListingFetched: bool,
+};
+
+export default SectionHowItWorks;
