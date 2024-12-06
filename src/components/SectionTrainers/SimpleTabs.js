a503ffff841c635f82ac0ca2f9267d29536a6a4d
Negin228 <99446649+Negin228@users.noreply.github.com>
2024-12-05 16:37:39 -0800

    Update SimpleTabs.js

--- src/components/SectionTrainers/SimpleTabs.js
+++ src/components/SectionTrainers/SimpleTabs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { Tabs, Tab } from 'react-tabs-scrollable';
+
 const SimpleTabs = props => {
   const { trainers = [], style, image, container } = props;
   const [activeTab, setActiveTab] = React.useState(1);
@@ -20,6 +21,7 @@ const SimpleTabs = props => {
         <Tab key={index}>
           {/* <img src={image} alt="amazon" className={imageStyle} key={index} /> */}
           <div className={container}>
+          className={css.imageHoverEffect}
             <img
               onClick={() => history.push(`/u/${trainer?.id?.uuid}`)}
               src={trainer.trainerProfileImage ? trainer.trainerProfileImage : image}
